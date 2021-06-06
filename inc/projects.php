f85b3680cb54a7f0b6ddd7be62bb5147fe6d7829
RundesBalli <GitHub@RundesBalli.com>
2021-06-07 00:16:24 +0200

    Aufteilung geändert, Factorio Blueprints hinzugefügt

--- inc/projects.php
+++ inc/projects.php
@@ -9,15 +9,29 @@ $title = "eigene Projekte";
 $content.= "<h1><span class='fas icon'>&#xf542;</span>eigene Projekte</h1>".PHP_EOL;
 $content.= "<div class='spacer-m'></div>".PHP_EOL;
 
-$content.= "<div class='row'>".PHP_EOL.
-"<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://github.com/RundesBalli/lightweight-PHP-Boilerplate' target='_blank' rel='noopener'><span class='fas icon'>&#xf0ad;</span>lightweight-PHP-Boilerplate</a></div>".PHP_EOL.
-"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Eine leichte und einfach anpassbare Code-Grundlage für die einfache Erstellung sicherer und dynamischer Webseiten.</div>".PHP_EOL.
-"</div>".PHP_EOL;
-$content.= "<div class='spacer-m'></div>".PHP_EOL;
+/**
+ * Internetpräsenzen
+ */
+$content.= "<h2>Internetpräsenzen</h2>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
 
 $content.= "<div class='row'>".PHP_EOL.
 "<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://php-noise.com' target='_blank' rel='noopener'><span class='fas icon'>&#xf1b3;</span>php-noise Bildgenerator</a></div>".PHP_EOL.
-"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Ein einfaches Script welches Hintergrundbilder mit Farbrauschen erstellt.</div>".PHP_EOL.
+"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Ein einfaches Script welches Hintergrundbilder mit Farbrauschen erstellt.<br>SourceCode bei <span class='fab icon'>&#xf09b;</span>GitHub: <a href='https://github.com/RundesBalli/php-noise.com' target='_blank' rel='noopener'>Frontend</a>, <a href='https://github.com/RundesBalli/php-noise' target='_blank' rel='noopener'>Backend</a></div>".PHP_EOL.
+"</div>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<hr>".PHP_EOL;
+
+/**
+ * Tools
+ */
+$content.= "<h2>Tools</h2>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<div class='row'>".PHP_EOL.
+"<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://github.com/RundesBalli/lightweight-PHP-Boilerplate' target='_blank' rel='noopener'><span class='fas icon'>&#xf0ad;</span>lightweight-PHP-Boilerplate</a></div>".PHP_EOL.
+"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Eine leichte und einfach anpassbare Code-Grundlage für die einfache Erstellung sicherer und dynamischer Webseiten.</div>".PHP_EOL.
 "</div>".PHP_EOL;
 $content.= "<div class='spacer-m'></div>".PHP_EOL;
 
@@ -37,23 +51,61 @@ $content.= "<div class='row'>".PHP_EOL.
 "<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://github.com/RundesBalli/terminal-imager' target='_blank' rel='noopener'><span class='fas icon'>&#xf302;</span>terminal-imager</a></div>".PHP_EOL.
 "<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Ein CLI-Script um Bilder in \"ANSI-Escape-Code-Bilder\" umzuwandeln, geschrieben in PHP.</div>".PHP_EOL.
 "</div>".PHP_EOL;
-$content.= "<div class='spacer-m'></div>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<hr>".PHP_EOL;
+
+/**
+ * Factorio
+ */
+$content.= "<h2>Factorio</h2>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<div class='row'>".PHP_EOL.
+"<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://github.com/RundesBalli/factorio-blueprints' target='_blank' rel='noopener'><span class='fas icon'>&#xf013;</span>Factorio Blueprints</a></div>".PHP_EOL.
+"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Eine Sammlung meiner Blaupausenbücher.</div>".PHP_EOL.
+"</div>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<hr>".PHP_EOL;
+
+/**
+ * Discord
+ */
+$content.= "<h2>Discord</h2>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
 
 $content.= "<div class='row'>".PHP_EOL.
 "<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://github.com/RundesBalli/discord-reminderBot' target='_blank' rel='noopener'><span class='fab icon'>&#xf392;</span>Discord Reminder-Bot</a></div>".PHP_EOL.
 "<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Script zum automatischen Senden eines vordefinierten Textes per WebHook an einen Textkanal.</div>".PHP_EOL.
 "</div>".PHP_EOL;
-$content.= "<div class='spacer-m'></div>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<hr>".PHP_EOL;
+
+/**
+ * Telegram
+ */
+$content.= "<h2>Telegram</h2>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
 
 $content.= "<div class='row'>".PHP_EOL.
 "<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://t.me/zufallbot' target='_blank' rel='noopener'><span class='fas icon'>&#xf522;</span>Telegram Zufallbot</a></div>".PHP_EOL.
 "<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Generiert Zufallsdaten und schickt sie an den anfragenden Telegram-Clienten.<br>SourceCode bei <a href='https://github.com/RundesBalli/telegram-Zufallbot' target='_blank' rel='noopener'><span class='fab icon'>&#xf09b;</span>GitHub</a>.</div>".PHP_EOL.
 "</div>".PHP_EOL;
-$content.= "<div class='spacer-m'></div>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
+
+$content.= "<hr>".PHP_EOL;
+
+/**
+ * Weiteres
+ */
+$content.= "<h2>Weiteres</h2>".PHP_EOL;
+$content.= "<div class='spacer-s'></div>".PHP_EOL;
 
 $content.= "<div class='row'>".PHP_EOL.
 "<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://github.com/RundesBalli' target='_blank' rel='noopener'><span class='fab icon'>&#xf09b;</span>GitHub</a></div>".PHP_EOL.
-"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Weitere Projekte auf Github.</div>".PHP_EOL.
+"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Weitere Projekte können auf Github eingesehen werden.</div>".PHP_EOL.
 "</div>".PHP_EOL;
 $content.= "<div class='spacer-l'></div>".PHP_EOL;
 
