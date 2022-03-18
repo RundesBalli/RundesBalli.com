61de46536c4acd1931b3b3350229547673430acf
RundesBalli <GitHub@RundesBalli.com>
2022-03-18 13:46:13 +0100

    Überschriften und Texte zentriert

--- public/inc/start.php
+++ public/inc/start.php
@@ -8,8 +8,8 @@
 /**
  * Willkommen
  */
-$content.= "<h1>Herzlich willkommen</h1>";
-$content.= "<div class='row'>".
+$content.= "<h1 class='center'>Herzlich willkommen</h1>";
+$content.= "<div class='row center'>".
 "<div class='col-s-12 col-l-12'>Herzlich willkommen auf meiner Internetpräsenz auf welcher ich meine Projekte vorstelle.</div>".
 "</div>";
 $content.= "<div class='row center'>".
@@ -20,7 +20,7 @@ $content.= "<div class='spacer-l'></div>";
 /**
  * Kontaktmöglichkeiten
  */
-$content.= "<h2>Kontaktmöglichkeiten</h2>";
+$content.= "<h2 class='center'>Kontaktmöglichkeiten</h2>";
 $content.= "<div class='row center'>".
 "<div class='col-s-12 col-l-12'><a href='https://t.me/RundesBalli' target='_blank' rel='noopener'><span class='fab icon'>&#xf2c6;</span>Telegram</a></div>".
 "<div class='col-s-12 col-l-12'><a href='https://pr0gramm.com/user/RundesBalli' target='_blank' rel='noopener'><span class='fas icon'>&#xf120;</span>pr0gramm</a></div>".
