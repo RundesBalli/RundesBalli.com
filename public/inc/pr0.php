56a301b73549b874ce20dfe6f53a9ec8b76b9c3b
RundesBalli <GitHub@RundesBalli.com>
2022-03-18 13:04:03 +0100

    Stahlofen Projekt entfernt

--- public/inc/pr0.php
+++ public/inc/pr0.php
@@ -24,11 +24,6 @@ $content.= "<div class='row'>".
 "<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://pr0p0ll.com' target='_blank' rel='noopener'><span class='fas icon'>&#xf044;</span>pr0p0ll</a></div>".
 "<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>pr0p0ll ist ein Umfragenportal für pr0gramm Mitglieder.</div>".
 "</div>";
-
-$content.= "<div class='row'>".
-"<div class='col-x-12 col-s-4 col-m-4 col-l-4 col-xl-4'><a href='https://stahlofen.info' target='_blank' rel='noopener'><span class='fas icon'>&#xf06d;</span>Stahlofen.info</a></div>".
-"<div class='col-x-12 col-s-8 col-m-8 col-l-8 col-xl-8'>Infoseite mit Verlinkung zu cha0s legendärem Kommentar.</div>".
-"</div>";
 $content.= "<div class='spacer-s'></div>";
 
 $content.= "<hr>";
