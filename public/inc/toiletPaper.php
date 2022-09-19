a7ca8d0a7a6d90b195d330e4c0dc4f0586eaeeab
RundesBalli <GitHub@RundesBalli.com>
2022-09-19 02:04:34 +0200

    Added layer option in calculator

--- public/inc/toiletPaper.php
+++ public/inc/toiletPaper.php
@@ -10,16 +10,20 @@ $content.= "<div class='row center'>".
 "</div>";
 $content.= "<form method='post'>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Gesamtpreis</div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 2,79' min='0.01' step='0.01' name='price'></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Gesamtpreis in €</div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 2,79' min='0.01' step='0.01' name='price' required></div>".
 "</div>";
 $content.= "<div class='row'>".
   "<div class='col-s-12 col-l-6 alignRightNotMobile'>Anzahl Rollen</div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 8' min='1' step='1' name='rolls'></div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 8' min='1' step='1' name='rolls' required></div>".
 "</div>";
 $content.= "<div class='row'>".
   "<div class='col-s-12 col-l-6 alignRightNotMobile'>Blatt pro Rolle</div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 150' min='100' step='10' name='sheets'></div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 150' min='100' step='10' name='sheets' required></div>".
+"</div>";
+$content.= "<div class='row'>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Lagen</div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 3' min='1' max='6' step='1' name='layers'></div>".
 "</div>";
 $content.= "<div class='row'>".
   "<div class='col-s-12 col-l-6 alignRightNotMobile'>Ausrechnen</div>".
@@ -36,6 +40,7 @@ if(isset($_POST['submit'])) {
   $price = abs(floatval(str_replace(',', '.', $_POST['price'])));
   $rolls = abs(intval($_POST['rolls']));
   $sheets = abs(intval($_POST['sheets']));
+  $layers = ((!empty($_POST['layers']) AND intval($_POST['layers']) > 0) ? intval($_POST['layers']) : 0);
 
   if(empty($price) OR empty($rolls) OR empty($sheets)) {
     return;
@@ -44,7 +49,15 @@ if(isset($_POST['submit'])) {
   $pricePer100Sheet = 100*($price/($rolls*$sheets));
   $content.= "<h3>Ergebnis</h3>";
   $content.= "<div class='row center'>".
-    "<div class='col-s-12 col-l-12'>100 Blatt kosten <span class='highlight bold'>".number_format($pricePer100Sheet, 4, ",", ".")."</span>.</div>".
+    "<div class='col-s-12 col-l-6 alignRightNotMobile'>Preis pro 100 Blatt</div>".
+    "<div class='col-s-12 col-l-6 bold highlight'>".number_format($pricePer100Sheet, 4, ",", ".")."€</div>".
   "</div>";
+  if($layers > 0) {
+    $pricePer1000Layers = $pricePer100Sheet/$layers*10;
+    $content.= "<div class='row center'>".
+      "<div class='col-s-12 col-l-6 alignRightNotMobile'>Preis pro 1000 einzelnen Lagen</div>".
+      "<div class='col-s-12 col-l-6 bold highlight'>".number_format($pricePer1000Layers, 4, ",", ".")."€</div>".
+    "</div>";
+  }
 }
 ?>
