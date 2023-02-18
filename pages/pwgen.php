aa76eb2da8fd1ce00b51579e0162e6889bb000b0
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 13:47:59 +0100

    Changed display of generated passwords

--- pages/pwgen.php
+++ pages/pwgen.php
@@ -5,19 +5,19 @@
  * Passwortgenerator
  */
 
-$title = "Passwortgenerator";
-$content.= "<h1><span class='fas icon'>&#xf084;</span>Passwortgenerator <code>[0-9a-zA-Z]{64}</code></h1>";
+$title = 'Passwortgenerator';
+$content.= '<h1><span class="fas icon">&#xf084;</span>Passwortgenerator <code>[0-9a-zA-Z]{64}</code></h1>';
 
-$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+$alphabet = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
 
-$content.= "<div class='row center'>";
+$content.= '<div class="center">';
 for($x = 0; $x < 100; $x++) {
-  $content.= "<div class='col-s-12 col-l-12 pre breakWord'>";
+  $content.= '<p class="pre">';
   for ($i = 0; $i < 64; $i++) {
     $n = mt_rand(0, strlen($alphabet)-1);
     $content.= $alphabet[$n];
   }
-  $content.= "</div>";
+  $content.= '</pre></p>';
 }
-$content.= "</div>";
+$content.= '</div>';
 ?>
