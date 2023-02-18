c89bb06e557bb1fe1830fefd32448a42af13e152
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 11:59:24 +0100

    Added constants and output function

--- includes/loader.php
+++ includes/loader.php
@@ -5,4 +5,10 @@
  * Configuration and function loader
  */
 
+/**
+ * Basic functions
+ */
+require_once(__DIR__.DIRECTORY_SEPARATOR.'constants.php');
+require_once(__DIR__.DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'output.php');
+
 ?>
