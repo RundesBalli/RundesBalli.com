f4d20245c32f389763c1d67e2e71ac4435c7d4fb
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 12:06:51 +0100

    Added routes

--- includes/routing/routes.php
+++ includes/routing/routes.php
@@ -7,12 +7,25 @@
  * @var array
  */
 $routes = [
+  /**
+   * Pages
+   */
+  'start' => 'start.php',
+  'imprint' => 'imprint.php',
+  'projects' => 'projects.php',
+  'pr0' => 'pr0.php',
+
+  /**
+   * Tools
+   */
+  'tools' => 'tools.php',
+  'pwgen' => 'pwgen.php',
+  'toiletPaper' => 'toiletPaper.php',
 
   /**
    * Error pages
    */
   '404' => 'errors'.DIRECTORY_SEPARATOR.'404.php',
-  '403' => 'errors'.DIRECTORY_SEPARATOR.'403.php',
-
+  '403' => 'errors'.DIRECTORY_SEPARATOR.'403.php'
 ];
 ?>
