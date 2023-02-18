1408fd42be71cb0a0cb583aad6fe848a1a783d0f
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 11:59:46 +0100

    Added tidyOutput function

--- includes/loader.php
+++ includes/loader.php
@@ -11,4 +11,5 @@
 require_once(__DIR__.DIRECTORY_SEPARATOR.'constants.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'output.php');
 
+require_once(__DIR__.DIRECTORY_SEPARATOR.'generation'.DIRECTORY_SEPARATOR.'tidyOutput.php');
 ?>
