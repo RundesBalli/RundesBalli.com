a5da6f275b31ca6628655bd5d0e77576de7efc20
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 12:01:49 +0100

    Added error pages to routes

---
+++ includes/routing/routes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * includes/routing/routes.php
+ * 
+ * Routes
+ * 
+ * @var array
+ */
+$routes = [
+
+  /**
+   * Error pages
+   */
+  '404' => 'errors'.DIRECTORY_SEPARATOR.'404.php',
+  '403' => 'errors'.DIRECTORY_SEPARATOR.'403.php',
+
+];
+?>
