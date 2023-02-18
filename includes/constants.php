61203e847f4f35a6acb376fa7368556cc496dcf5
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 15:19:35 +0100

    Added badges

--- includes/constants.php
+++ includes/constants.php
@@ -25,4 +25,17 @@ const PAGE_INCLUDE_DIR = INCLUDE_DIR.'..'.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_
  * The time period during which the cookies are valid, or the time until which they are extended.
  */
 const COOKIE_DURATION = 86400*45;
+
+/**
+ * Language badges
+ */
+define('BADGE', [
+  'php' => '<span class="fab icon help" title="PHP">&#xf457;</span>',
+  'html5' => '<span class="fab icon help" title="HTML5">&#xf13b;</span>',
+  'css3' => '<span class="fab icon help" title="CSS3">&#xf13c;</span>',
+  'sql' => '<span class="fas icon help" title="SQL as database">&#xf1c0;</span>',
+  'api' => '<span class="fas icon help" title="provides an API">&#xf6ff;</span>',
+  'cli' => '<span class="fas icon help" title="CLI script">&#xf120;</span>',
+  'telegram' => '<span class="fab icon help" title="Telegram bot">&#xf2c6;</span>'
+]);
 ?>
