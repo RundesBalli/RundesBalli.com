a915e873da50032d19ec756285f15566d9b818f8
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 16:57:46 +0100

    Translated toiletpaper calculator to English

--- pages/toiletPaper.php
+++ pages/toiletPaper.php
@@ -4,7 +4,8 @@
  * 
  * Toilet paper calculator
  */
-$content.= "<h1><span class='fas icon'>&#xf71e;</span>Toilettenpapier Rechner</h1>";
+$title = 'Toilet Paper Calculator';
+$content.= "<h1><span class='fas icon'>&#xf71e;</span>Toilet Paper Calculator</h1>";
 
 /**
  * Calculate
@@ -22,12 +23,12 @@ if(isset($_POST['submit'])) {
   $pricePer100Sheet = 100*($price/($rolls*$sheets));
   $content.= "<h3>Ergebnis</h3>";
   $content.= "<div class='row'>".
-    "<div class='col-s-12 col-l-6 alignRightNotMobile'>Preis pro 100 Blatt</div>".
+    "<div class='col-s-12 col-l-6 alignRightNotMobile'>Price per 100 Sheets</div>".
     "<div class='col-s-12 col-l-6 bold highlight'>".number_format($pricePer100Sheet, 4, ",", ".")."€</div>".
   "</div>";
   $pricePer1000Layers = $pricePer100Sheet/$layers*10;
   $content.= "<div class='row'>".
-    "<div class='col-s-12 col-l-6 alignRightNotMobile'>Preis pro 1000 einzelnen Lagen</div>".
+    "<div class='col-s-12 col-l-6 alignRightNotMobile'>Price per 1000 Layers</div>".
     "<div class='col-s-12 col-l-6 bold highlight'>".number_format($pricePer1000Layers, 4, ",", ".")."€</div>".
   "</div>";
   $addNew = TRUE;
@@ -40,36 +41,36 @@ if(isset($_POST['submit'])) {
 $tabindex = 1;
 $content.= "<form method='post'>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Gesamtpreis in € <span class='note highlight'>*</span></div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 3,79' min='0.01' step='0.01' name='price' tabindex='".$tabindex++."' required autofocus></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Total price in € <span class='note highlight'>*</span></div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='e.g. 3,79' min='0.01' step='0.01' name='price' tabindex='".$tabindex++."' required autofocus></div>".
 "</div>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Anzahl Rollen <span class='note highlight'>*</span></div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 8' min='1' step='1' name='rolls' tabindex='".$tabindex++."' required></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Roll count <span class='note highlight'>*</span></div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='e.g. 8' min='1' step='1' name='rolls' tabindex='".$tabindex++."' required></div>".
 "</div>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Blatt pro Rolle <span class='note highlight'>*</span></div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 150' min='10' step='5' name='sheets' tabindex='".$tabindex++."' required></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Sheets per roll <span class='note highlight'>*</span></div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='e.g. 150' min='10' step='5' name='sheets' tabindex='".$tabindex++."' required></div>".
 "</div>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Lagen <span class='note highlight'>*</span></div>".
-  "<div class='col-s-12 col-l-6'><input type='number' placeholder='Bspw. 3' min='1' max='6' step='1' name='layers' tabindex='".$tabindex++."' required></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Layers <span class='note highlight'>*</span></div>".
+  "<div class='col-s-12 col-l-6'><input type='number' placeholder='e.g. 3' min='1' max='6' step='1' name='layers' tabindex='".$tabindex++."' required></div>".
 "</div>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Bezeichnung <span class='note highlight'>**</span></div>".
-  "<div class='col-s-12 col-l-6'><input type='text' placeholder='Billopapier' maxlength='40' name='desc' tabindex='".$tabindex++."'></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Description / Name <span class='note highlight'>**</span></div>".
+  "<div class='col-s-12 col-l-6'><input type='text' placeholder='Shitpaper' maxlength='40' name='desc' tabindex='".$tabindex++."'></div>".
 "</div>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Angebotspreis?</div>".
-  "<div class='col-s-12 col-l-6'><select name='off' tabindex='".$tabindex++."'><option value='1'>Ja</option><option value='0' selected>Nein</option></select></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Special offer price?</div>".
+  "<div class='col-s-12 col-l-6'><select name='off' tabindex='".$tabindex++."'><option value='1'>Yes</option><option value='0' selected>No</option></select></div>".
 "</div>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Ausrechnen</div>".
-  "<div class='col-s-12 col-l-6'><input type='submit' name='submit' value='Ausrechnen' tabindex='".$tabindex++."'></div>".
+  "<div class='col-s-12 col-l-6 alignRightNotMobile'>Calculate</div>".
+  "<div class='col-s-12 col-l-6'><input type='submit' name='submit' value='Calculate' tabindex='".$tabindex++."'></div>".
 "</div>";
 $content.= "<div class='row note center'>".
-  "<div class='col-s-12 col-l-12'><span class='highlight'>*</span> Erforderlich</div>".
-  "<div class='col-s-12 col-l-12'><span class='highlight'>**</span> Wenn eine Bezeichnung angegeben wird, wird der Eintrag zur späteren Verwendung in einem Cookie gespeichert.</div>".
+  "<div class='col-s-12 col-l-12'><span class='highlight'>*</span> Required</div>".
+  "<div class='col-s-12 col-l-12'><span class='highlight'>**</span> If a name is given, the entry is stored in a cookie for later use.</div>".
 "</div>";
 $content.= "</form>";
 $content.= "<div class='spacer-m'></div>";
@@ -116,7 +117,7 @@ if(!empty($_COOKIE['toiletPaper'])) {
      */
     if(!empty($addNew) AND $addNew === TRUE AND !empty($_POST['desc'])) {
       $tpVal[] = [
-        'desc' => strip_tags($_POST['desc']).'; Rol.:'.$rolls.'; Bl.:'.$sheets.'; Lag.:'.$layers,
+        'desc' => strip_tags($_POST['desc']).'; Rolls:'.$rolls.'; Sheets:'.$sheets.'; Layers:'.$layers,
         '100s' => $pricePer100Sheet,
         '1000l' => $pricePer1000Layers,
         'off' => ((!empty($_POST['off']) AND $_POST['off'] == 1) ? TRUE : FALSE),
@@ -137,10 +138,10 @@ if(!empty($_COOKIE['toiletPaper'])) {
        * Table heading
        */
       $content.= "<div class='row highlight bold marginBottomMobile'>".
-        "<div class='col-s-12 col-l-7'>Bezeichnung</div>".
-        "<div class='col-s-5 col-l-2'>100 Blatt</div>".
-        "<div class='col-s-5 col-l-2'>1000 Lagen</div>".
-        "<div class='col-s-12 col-l-1'>Angebotspreis</div>".
+        "<div class='col-s-12 col-l-7'>Description / Name</div>".
+        "<div class='col-s-5 col-l-2'>100 Sheets</div>".
+        "<div class='col-s-5 col-l-2'>1000 Layers</div>".
+        "<div class='col-s-12 col-l-1'>Special offer price</div>".
       "</div>";
 
       /**
@@ -167,7 +168,7 @@ if(!empty($_COOKIE['toiletPaper'])) {
    */
   if(!empty($addNew) AND $addNew === TRUE AND !empty($_POST['desc'])) {
     setcookie('toiletPaper', json_encode([[
-      'desc' => strip_tags($_POST['desc']).'; Rol.:'.$rolls.'; Bl.:'.$sheets.'; Lag.:'.$layers,
+      'desc' => strip_tags($_POST['desc']).'; Rolls:'.$rolls.'; Sheets:'.$sheets.'; Layers:'.$layers,
       '100s' => $pricePer100Sheet,
       '1000l' => $pricePer1000Layers,
       'off' => ((!empty($_POST['off']) AND $_POST['off'] == 1) ? TRUE : FALSE),
