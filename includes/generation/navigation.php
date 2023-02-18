b5e6b42ff3f7b0fee832dad57e1193dfc2bedb16
RundesBalli <GitHub@RundesBalli.com>
2023-02-18 17:50:36 +0100

    Changed old route variable to $route

--- includes/generation/navigation.php
+++ includes/generation/navigation.php
@@ -5,7 +5,7 @@
  * Navigation generation
  */
 $a = ' class="active"';
-$nav = '<a href="/"'.($getp == 'start' ? $a : NULL).'><span class="fas icon">&#xf015;</span>Home</a>';
-$nav.= '<a href="/projects"'.($getp == 'projects' ? $a : NULL).'><span class="fas icon">&#xf542;</span>Projects</a>';
-$nav.= '<a href="/pr0"'.($getp == 'pr0' ? $a : NULL).'><span class="fas icon">&#xf120;</span>pr0gramm</a>';
+$nav = '<a href="/"'.($route == 'start' ? $a : NULL).'><span class="fas icon">&#xf015;</span>Home</a>';
+$nav.= '<a href="/projects"'.($route == 'projects' ? $a : NULL).'><span class="fas icon">&#xf542;</span>Projects</a>';
+$nav.= '<a href="/pr0"'.($route == 'pr0' ? $a : NULL).'><span class="fas icon">&#xf120;</span>pr0gramm</a>';
 ?>
