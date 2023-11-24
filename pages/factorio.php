06bf93dbbfbaa28f5a8bc047c6779bd14826bbdc
RundesBalli <GitHub@RundesBalli.com>
2023-11-25 00:37:06 +0100

    Added some new factorio blueprints and replaced images

--- pages/factorio.php
+++ pages/factorio.php
@@ -37,36 +37,51 @@ $content.= '<div class="row center">'.
 $content.= '<div class="spacer-l"></div>';
 
 $content.= '<div class="row vCenter">'.
-  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/electric-mining-drill-arrays" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/1-array,red.png" alt="Electric mining drill arrays"></a></div>'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/electric-mining-drill-arrays" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/miningArrays.png" alt="Electric mining drill arrays"></a></div>'.
   '<div class="col-s-12 col-l-7">Space saving setup of electric mining drills in various array-sizes and all belt speeds.</div>'.
-  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/electric-mining-drill-arrays" target="_blank" rel="noopener">Blueprint book</a></div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/electric-mining-drill-arrays" target="_blank" rel="noopener">Blueprint book</a></div>'.
 '</div>';
 $content.= '<div class="spacer-l"></div>';
 
 $content.= '<div class="row vCenter">'.
-  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/rail-system" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/intersection-l,ss.png" alt="Rail system"></a></div>'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/rail-system" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/rail.png" alt="Rail system"></a></div>'.
   '<div class="col-s-12 col-l-7">Right-hand rail system for five part trains.</div>'.
-  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/rail-system" target="_blank" rel="noopener">Blueprint book</a></div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/rail-system" target="_blank" rel="noopener">Blueprint book</a></div>'.
 '</div>';
 $content.= '<div class="spacer-l"></div>';
 
 $content.= '<div class="row vCenter">'.
-  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/furnaces" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/48electric-furnaces,red,opposite.png" alt="Furnaces"></a></div>'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/furnaces" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/furnaces.png" alt="Furnaces"></a></div>'.
   '<div class="col-s-12 col-l-7">Various furnace setups.</div>'.
-  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/furnaces" target="_blank" rel="noopener">Blueprint book</a></div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/furnaces" target="_blank" rel="noopener">Blueprint book</a></div>'.
 '</div>';
 $content.= '<div class="spacer-l"></div>';
 
 $content.= '<div class="row vCenter">'.
-  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/7-segment-displays" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/L,8digits,green.png" alt="7-Segment-Displays"></a></div>'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/7-segment-displays" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/7segment.png" alt="7-Segment-Displays"></a></div>'.
   '<div class="col-s-12 col-l-7">7-segment-displays in various sizes and digit counts.</div>'.
-  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/7-segment-displays" target="_blank" rel="noopener">Blueprint book</a></div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/7-segment-displays" target="_blank" rel="noopener">Blueprint book</a></div>'.
 '</div>';
 $content.= '<div class="spacer-l"></div>';
 
 $content.= '<div class="row vCenter">'.
-  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/kovarex-enrichment-process" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/kovarex.png" alt="Automatic Kovarex Enrichment Process"></a></div>'.
-  '<div class="col-s-12 col-l-7">Automatic Kovarex Enrichment Process with automatic feeding of additional machines when the first machines are running at the limit.</div>'.
-  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/blob/master/kovarex-enrichment-process" target="_blank" rel="noopener">Blueprint</a></div>'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/power-sources" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/power.png" alt="Power sources"></a></div>'.
+  '<div class="col-s-12 col-l-7">Some early- and lategame power sources.</div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/power-sources" target="_blank" rel="noopener">Blueprint book</a></div>'.
 '</div>';
+$content.= '<div class="spacer-l"></div>';
+
+$content.= '<div class="row vCenter">'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/kovarex-enrichment-process" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/kovarex.png" alt="Automatic Kovarex Enrichment Process"></a></div>'.
+  '<div class="col-s-12 col-l-7">Automatic Kovarex Enrichment Process with automatic feeding of additional machines when the first machines are running at the limit.</div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/kovarex-enrichment-process" target="_blank" rel="noopener">Blueprints</a></div>'.
+'</div>';
+$content.= '<div class="spacer-l"></div>';
+
+$content.= '<div class="row vCenter">'.
+  '<div class="col-s-12 col-l-3 bold"><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/nuclear-power" target="_blank" rel="noopener"><img class="imgGlow" src="/assets/images/factorio/nuclearPowerPlant.png" alt="480 MW Nuclear Power Plant"></a></div>'.
+  '<div class="col-s-12 col-l-7">480 MW Nuclear Power Plant.</div>'.
+  '<div class="col-s-12 col-l-2"><span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli/factorio-blueprints/tree/master/nuclear-power" target="_blank" rel="noopener">Blueprint</a></div>'.
+'</div>';
+$content.= '<div class="spacer-l"></div>';
 ?>
