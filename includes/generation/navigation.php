5cccca56ffc0d40d73c4faca8820a4eb32522891
RundesBalli <GitHub@RundesBalli.com>
2025-02-24 01:03:17 +0100

    Added some navigation links

--- includes/generation/navigation.php
+++ includes/generation/navigation.php
@@ -5,6 +5,46 @@
  * Navigation generation
  */
 $navElements = [
+  [
+    'faSet' => 'far',
+    'faIcon' => 'f2c2',
+    'url' => '/',
+    'route' => 'default',
+    'text' => 'About me',
+    'newTab' => FALSE,
+  ],
+  [
+    'faSet' => 'fas',
+    'faIcon' => 'f542',
+    'url' => '/projects',
+    'route' => 'projects',
+    'text' => 'Projects',
+    'newTab' => FALSE,
+  ],
+  [
+    'faSet' => 'fas',
+    'faIcon' => 'f072',
+    'url' => '/adsb',
+    'route' => 'adsb',
+    'text' => 'ADS-B',
+    'newTab' => FALSE,
+  ],
+  [
+    'faSet' => 'fas',
+    'faIcon' => 'f013',
+    'url' => '/factorio',
+    'route' => 'factorio',
+    'text' => 'Factorio',
+    'newTab' => FALSE,
+  ],
+  [
+    'faSet' => 'fas',
+    'faIcon' => 'f7d9',
+    'url' => '/tools',
+    'route' => 'tools',
+    'text' => 'Online Tools',
+    'newTab' => FALSE,
+  ],
 ];
 
 $nav = '<a id="toggleElement"></a>';
