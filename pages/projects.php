d939f10f0666e7e68806e422b72ff517b0d4b2da
RundesBalli <GitHub@RundesBalli.com>
2025-02-24 21:51:44 +0100

    Added project list.
    WIP: List/View

---
+++ pages/projects.php
@@ -0,0 +1,460 @@
+<?php
+/**
+ * pages/projects.php
+ * 
+ * Project page
+ */
+
+$title = 'Projects';
+$content.= '<h1><span class="fas icon">&#xf542;</span>Projects</h1>';
+
+/**
+ * OG Tags
+ */
+$ogDescription = 'A collection of all my public projects. Websites, tools, scripts, telegram bots, snippets and more...';
+
+/**
+ * Project array
+ */
+$projects = [
+  /**
+   * Websites
+   */
+  [
+    'title' => 'Websites',
+    'faClass' => 'fas',
+    'faSymbol' => 'f0ac',
+    'projects' => [
+      /**
+       * PHP-Noise website
+       */
+      [
+        'title' => 'PHP-Noise.com',
+        'faClass' => 'fas',
+        'faSymbol' => 'f03e',
+        'url' => 'https://php-noise.com',
+        'description' => 'Generate random noisy images as background images for websites, flyers and so on.',
+        'sourceUrl' => 'https://github.com/RundesBalli/php-noise.com',
+        'badges' => [
+          'php',
+          'html5',
+          'css3',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+
+      /**
+       * PHP-Noise CLI/API
+       */
+      [
+        'title' => 'PHP-Noise CLI',
+        'faClass' => 'fas',
+        'faSymbol' => 'f03e',
+        'url' => NULL,
+        'description' => 'The CLI script and API for the above mentioned PHP-Noise website.',
+        'sourceUrl' => 'https://github.com/RundesBalli/php-noise',
+        'badges' => [
+          'php',
+          'cli',
+          'api',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+
+      /**
+       * Tankersparnis.net
+       */
+      [
+        'title' => 'Tankersparnis.net',
+        'faClass' => 'fas',
+        'faSymbol' => 'f52f',
+        'url' => 'https://tankersparnis.net',
+        'description' => 'German portal for easy calculation of money saved when refueling with LPG compared to conventional fuels.',
+        'sourceUrl' => 'https://github.com/RundesBalli/tankersparnis',
+        'badges' => [
+          'php',
+          'html5',
+          'css3',
+          'sql',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+
+  /**
+   * Projects of friends I have collaborated on
+   */
+  [
+    'title' => 'Projects of friends I have collaborated on',
+    'faClass' => 'fas',
+    'faSymbol' => 'e533',
+    'projects' => [
+      /**
+       * Nezos.Cooking
+       */
+      [
+        'title' => 'Nezos.Cooking',
+        'faClass' => 'fas',
+        'faSymbol' => 'f543',
+        'url' => 'https://nezos.cooking',
+        'description' => 'German recipe collection from my friend <a href="https://nezos.wtf" target="_blank" rel="noopener">Nezos</a>.',
+        'sourceUrl' => 'https://github.com/RundesBalli/cooking',
+        'badges' => [
+          'php',
+          'html5',
+          'css3',
+          'sql',
+        ],
+        'tasks' => 'Backend and Frontend',
+        'note' => 'The recipes are not from me.',
+      ],
+
+      /**
+       * Boomerparty.live
+       */
+      [
+        'title' => 'Boomerparty.live',
+        'faClass' => 'fas',
+        'faSymbol' => 'f144',
+        'url' => 'https://boomerparty.live',
+        'description' => 'Official website for the Boomerparty-Playlist on <a href="https://open.spotify.com/playlist/64WfneVI8dqmpd6T6QKAMs" target="_blank" rel="noopener">Spotify</a> and <a href="https://www.youtube.com/playlist?list=PLOnRDACaCcSMom4d1cAxxaoFjeP9167I8" target="_blank" rel="noopener">YouTube</a>. A community project of my friend <a href="https://pr0gramm.com/user/KaiserMolay" target="_blank" rel="noopener">KaiserMolay</a>.',
+        'sourceUrl' => 'https://github.com/RundesBalli/boomerparty.live',
+        'badges' => [
+          'php',
+          'html5',
+          'css3',
+        ],
+        'tasks' => 'Whole website without photographs and images.',
+        'note' => 'The playlist is maintained by KaiserMolay.',
+      ],
+    ],
+  ],
+
+  /**
+   * Satire and Fun
+   */
+  [
+    'title' => 'Satire and Fun',
+    'faClass' => 'fas',
+    'faSymbol' => 'f59b',
+    'projects' => [
+      /**
+       * Anti-Ananas Club
+       */
+      [
+        'title' => 'Anti-Ananas Club',
+        'faClass' => 'fas',
+        'faSymbol' => 'f00d',
+        'url' => 'https://anti-ananas.club',
+        'description' => 'A German satire counter-reaction project to the Hawaii pizza meme.',
+        'sourceUrl' => 'https://github.com/RundesBalli/anti-ananas.club',
+        'badges' => [
+          'php',
+          'html5',
+          'css3',
+        ],
+        'tasks' => NULL,
+        'note' => '„Ananas“ = Pineapple',
+      ],
+
+      /**
+       * Anti-Ananas Club pr0gramm bot
+       */
+      [
+        'title' => 'Anti-Ananas pr0gramm bot',
+        'faClass' => 'fas',
+        'faSymbol' => 'f544',
+        'url' => NULL,
+        'description' => 'Anti-Ananas.Club comment-autoresponse bot for the German imageboard pr0gramm.com',
+        'sourceUrl' => 'https://github.com/RundesBalli/pr0gramm-antiAnanas',
+        'badges' => [
+          'php',
+          'api',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+
+      /**
+       * Pfandautomat.com
+       */
+      [
+        'title' => 'Pfandautomat.com',
+        'faClass' => 'fas',
+        'faSymbol' => 'f72f',
+        'url' => 'https://pfandautomat.com',
+        'description' => 'A little German fun project about the very annoying oneway-bottle deposit machines (= „Pfandautomat“) known in Germany.',
+        'sourceUrl' => 'https://github.com/RundesBalli/Pfandautomat.com',
+        'badges' => [
+          'php',
+          'html5',
+          'css3',
+          'js',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+
+  /**
+   * Scripts
+   */
+  [
+    'title' => 'Scripts',
+    'faClass' => 'fas',
+    'faSymbol' => 'f70e',
+    'projects' => [
+      [
+        'title' => 'lightweight-PHP-Boilerplate',
+        'faClass' => 'fas',
+        'faSymbol' => 'f0ad',
+        'url' => NULL,
+        'description' => 'A lightweight PHP boilerplate and customizable code-foundation for building secure and dynamic websites with ease.',
+        'sourceUrl' => 'https://github.com/RundesBalli/lightweight-PHP-Boilerplate',
+        'badges' => [
+          'php',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'escpos-php-driver',
+        'faClass' => 'fas',
+        'faSymbol' => 'f02f',
+        'url' => NULL,
+        'description' => 'A driver for Epson <code>ESC/POS</code> thermal printers.',
+        'sourceUrl' => 'https://github.com/RundesBalli/escpos-php-driver',
+        'badges' => [
+          'php',
+          'cli',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'terminal-imager',
+        'faClass' => 'fas',
+        'faSymbol' => 'f302',
+        'url' => NULL,
+        'description' => 'A CLI script to convert images to <code>ANSI escape code</code> images.',
+        'sourceUrl' => 'https://github.com/RundesBalli/terminal-imager',
+        'badges' => [
+          'php',
+          'cli',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'jsonDumper',
+        'faClass' => 'fas',
+        'faSymbol' => 'f793',
+        'url' => NULL,
+        'description' => 'Simple JSON dumper with token and file listing.',
+        'sourceUrl' => 'https://github.com/RundesBalli/jsonDumper',
+        'badges' => [
+          'php',
+          'json',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+
+  /**
+   * Snippets
+   */
+  [
+    'title' => 'Snippets',
+    'faClass' => 'fas',
+    'faSymbol' => 'f121',
+    'projects' => [
+      [
+        'title' => 'clickableLinks',
+        'faClass' => 'fas',
+        'faSymbol' => 'f0c1',
+        'url' => NULL,
+        'description' => 'Function to automatically link URLs in texts.',
+        'sourceUrl' => 'https://github.com/RundesBalli/phpSnippets/tree/master/clickableLinks',
+        'badges' => [
+          'php',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'hex2rgb',
+        'faClass' => 'fas',
+        'faSymbol' => '23',
+        'url' => NULL,
+        'description' => 'Simple PHP function based on the <a href="https://www.php.net/manual/en/function.hexdec.php" target="_blank" rel="noopener">hexdec</a> function and RegEx.',
+        'sourceUrl' => 'https://github.com/RundesBalli/phpSnippets/tree/master/hex2rgb',
+        'badges' => [
+          'php',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'Tidy function call',
+        'faClass' => 'fas',
+        'faSymbol' => 'f51a',
+        'url' => NULL,
+        'description' => 'Parameterised function call of the tidy function.',
+        'sourceUrl' => 'https://github.com/RundesBalli/phpSnippets/blob/master/tidyOutput',
+        'badges' => [
+          'php',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'Fractionizer',
+        'faClass' => 'fas',
+        'faSymbol' => '25',
+        'url' => NULL,
+        'description' => 'Simple number format function with quarter, half and three quarter fraction output.',
+        'sourceUrl' => 'https://github.com/RundesBalli/phpSnippets/tree/master/fractionizer',
+        'badges' => [
+          'php',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'formatTime',
+        'faClass' => 'fas',
+        'faSymbol' => 'f017',
+        'url' => NULL,
+        'description' => 'Simple PHP function to format a given time in seconds to a readable time.',
+        'sourceUrl' => 'https://github.com/RundesBalli/phpSnippets/tree/master/formatTime',
+        'badges' => [
+          'php',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+
+  /**
+   * Telegram bots
+   */
+  [
+    'title' => 'Telegram Bots',
+    'faClass' => 'fab',
+    'faSymbol' => 'f2c6',
+    'projects' => [
+      [
+        'title' => 'Random data bot',
+        'faClass' => 'fas',
+        'faSymbol' => 'f522',
+        'url' => 'https://t.me/zufallbot',
+        'description' => 'German random number, letter and lottery number generator.',
+        'sourceUrl' => 'https://github.com/RundesBalli/telegram-Zufallbot',
+        'badges' => [
+          'php',
+          'api',
+          'telegram',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'Chat-Id bot',
+        'faClass' => 'fas',
+        'faSymbol' => 'f47f',
+        'url' => 'https://t.me/rb_chatId_bot',
+        'description' => 'Returns the Chat ID in Groups and private chats.',
+        'sourceUrl' => 'https://github.com/RundesBalli/telegramChatIdBot',
+        'badges' => [
+          'php',
+          'api',
+          'telegram',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+      [
+        'title' => 'Server online-status check with reporting to telegram',
+        'faClass' => 'fas',
+        'faSymbol' => 'f06e',
+        'url' => NULL,
+        'description' => 'Simple <code>netcat</code> script to check the online status of certain services on a remote server.',
+        'sourceUrl' => 'https://gist.github.com/RundesBalli/4bdcf555c78c17a7b917de31cd7b4df0',
+        'badges' => [
+          'php',
+          'api',
+          'cli',
+          'telegram',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+
+  /**
+   * Crawler
+   */
+  [
+    'title' => 'Crawler',
+    'faClass' => 'fas',
+    'faSymbol' => 'f717',
+    'projects' => [
+      [
+        'title' => 'ADR Crawler',
+        'faClass' => 'fas',
+        'faSymbol' => 'f7ba',
+        'url' => NULL,
+        'description' => 'Ambient Dose Rate (German: ODL = Ortsdosisleistung) crawler for ~1700 monitoring sites within Germany and ~110 monitoring sites within austria.',
+        'sourceUrl' => 'https://github.com/RundesBalli/ADR-Crawler',
+        'badges' => [
+          'php',
+          'cli',
+          'api',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+
+  /**
+   * Discord
+   */
+  [
+    'title' => 'Discord',
+    'faClass' => 'fab',
+    'faSymbol' => 'f392',
+    'projects' => [
+      [
+        'title' => 'Reminder Bot',
+        'faClass' => 'fas',
+        'faSymbol' => 'f0a1',
+        'url' => NULL,
+        'description' => 'Script to automatically sends a predefined text as a bot or via WebHook to a text channel.',
+        'sourceUrl' => 'https://github.com/RundesBalli/discord-reminderBot',
+        'badges' => [
+          'php',
+          'cli',
+          'discord',
+        ],
+        'tasks' => NULL,
+        'note' => NULL,
+      ],
+    ],
+  ],
+];
+
+/**
+ * More
+ */
+$content.= 'Many of my projects are available on <span class="fab icon">&#xf09b;</span><a href="https://github.com/RundesBalli" target="_blank" rel="noopener">GitHub</a>.';
+?>
