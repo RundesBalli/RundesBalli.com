7dfb432d9e3c4d37862ea4cda3eb6429377904c3
RundesBalli <GitHub@RundesBalli.com>
2025-03-14 21:19:50 +0100

    Added openrct2 basic page

---
+++ pages/openrct2.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * pages/openrct2.php
+ * 
+ * OpenRCT2 project page
+ */
+
+$title = 'OpenRCT2 Projects';
+$content.= '<h1><span class="fas icon">&#xf546;</span>OpenRCT2 Projects</h1>';
+
+/**
+ * OG Tags
+ */
+$ogDescription = 'A collection of my OpenRCT2 coasters.';
+
+/**
+ * Project array
+ */
+$availableProjects = [
+?>
